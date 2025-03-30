9fe97554c0726d7b2fbf612e7115dd9302b4c61e
Johan Lopez <johanpppp222345@gmail.com>
2025-03-30 00:25:38 -0500

    last commit

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import path from "path"; // Import the path module
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/johan-lopez-torres",
-  resolve: {
+  base: "./",
+  resolve: {  
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
